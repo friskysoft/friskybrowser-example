3b068b4f0bf3a996f96ba345ee3d5c8df174ca5c
Rafaat Hossain <hossain@adobe.com>
2017-03-11 16:52:38 -0500

    using ChromeDriverManager

--- src/test/java/tests/BaseTestScript.java
+++ src/test/java/tests/BaseTestScript.java
@@ -5,19 +5,14 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.WebDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class BaseTestScript {
     public static Browser browser;
 
     @BeforeClass
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
-        browser = Browser.newInstance(driver);
+        ChromeDriverManager.getInstance().setup();
+        browser = Browser.newInstance(BrowserType.CHROME);
     }
 
     @AfterClass
